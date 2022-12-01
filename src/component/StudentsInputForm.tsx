2b4d8cc5a1ed54edb62c41b1c95a0b5f5d1ae0a2
FE_HT_VinhDT <vinhdt@hybrid-technologies.vn>
2022-12-01 17:39:38 +0700

    validate

--- src/component/StudentsInputForm.tsx
+++ src/component/StudentsInputForm.tsx
@@ -1,26 +1,40 @@
 import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import DatePicker from 'react-datepicker'
+import { useForm } from 'react-hook-form'
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify'
+import Moment from 'react-moment'
 
 export default function StudentsInputForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
   const [inputList, setInputList] = useState([
-    { fullName: '', dateOfBirth: '' },
+    { fullName: '', dateOfBirth: '', age: 0 },
   ])
+  const [isViewList, setIsViewList] = useState(false)
   const URL = 'http://localhost:3000/students'
-
-  const handleInputChange = (e, index) => {
-    const { name, value } = e.target
+  const handleInputChange = (e, name, index) => {
+    const { value } = e.target
     const list = [...inputList]
     list[index][name] = value
     setInputList(list)
   }
 
   const submitData = () => {
+    if (inputList.length < 30) {
+      toast.warning('Please input >= 30 students !')
+
+      return
+    }
+
     axios
       .post(URL, inputList)
       .then(() => {
+        setIsViewList(true)
         toast.success('Successfully !')
       })
       .catch((error) => {
@@ -40,70 +54,120 @@ export default function StudentsInputForm() {
     list.splice(index, 1)
     setInputList(list)
   }
-
   const handLeadClick = () => {
-    setInputList([...inputList, { fullName: '', dateOfBirth: '' }])
+    setInputList([...inputList, { fullName: '', dateOfBirth: '', age: 0 }])
   }
   return (
     <div>
       <ToastContainer />
       <Container className="content">
-        <div className="row">
-          <div className="col-sm-12">
-            <h5 className="mt-3 mb-4 fw-bold">
-              Dynamically add/remove students
-            </h5>
-
-            {inputList.map((x, i) => {
-              return (
-                <div className="row mb-3">
-                  <div className="form-group col-md-4">
-                    <label>Full name</label>
-                    <input
-                      type="text"
-                      name="fullName"
-                      className="form-control"
-                      onChange={(e) => handleInputChange(e, i)}
-                    />
-                  </div>
-                  <div className="form-group col-md-4">
-                    <label>Date of Birth - DOB</label>
-                    <DatePicker
-                      selected={x.dateOfBirth}
-                      className="form-control"
-                      onChange={(date: Date) => setStartDate(date, i)}
-                    />
-                  </div>
-                  <div className="form-group col-md-2 mt-4">
-                    {inputList.length !== 1 && (
-                      <button
-                        className="btn btn-danger mx-1"
-                        onClick={() => handleRemove(i)}
-                      >
-                        Remove
-                      </button>
-                    )}
-                    {inputList.length - 1 === i && (
-                      <button
-                        className="btn btn-success"
-                        onClick={handLeadClick}
-                      >
-                        Add More
-                      </button>
-                    )}
-                  </div>
-                </div>
-              )
-            })}
-          </div>
+        {!isViewList && (
           <div className="row">
             <div className="col-sm-12">
-              <button className="btn btn-success" onClick={submitData}>
-                Save
-              </button>
+              <h5 className="mt-3 mb-4 fw-bold">
+                Dynamically add/remove students
+              </h5>
+              <form onSubmit={handleSubmit(submitData)}>
+                {inputList.map((x, i) => {
+                  return (
+                    <div key={i} className="row mb-3">
+                      <div className="form-group col-md-3">
+                        <label>Full name *</label>
+                        <input
+                          key={'fullname' + i}
+                          {...register('fullName' + i, {
+                            required: true,
+                          })}
+                          className="form-control"
+                          onChange={(e) => handleInputChange(e, 'fullName', i)}
+                        />
+                        {errors['fullName' + i] && (
+                          <span className="text-danger">
+                            This field is required
+                          </span>
+                        )}
+                      </div>
+                      <div className="form-group col-md-3">
+                        <label>Date of Birth - DOB</label>
+                        <DatePicker
+                          selected={x.dateOfBirth}
+                          className="form-control"
+                          onChange={(date: Date) => setStartDate(date, i)}
+                        />
+                      </div>
+                      <div className="form-group col-md-2">
+                        <label>Age</label>
+                        <input
+                          type="number"
+                          name="age"
+                          className="form-control"
+                          onChange={(e) => handleInputChange(e, 'age', i)}
+                        />
+                      </div>
+                      <div className="form-group col-md-2 mt-4">
+                        {inputList.length !== 1 && (
+                          <button
+                            className="btn btn-danger mx-1"
+                            onClick={() => handleRemove(i)}
+                          >
+                            Remove
+                          </button>
+                        )}
+                        {inputList.length - 1 === i && (
+                          <button
+                            className="btn btn-success"
+                            onClick={handLeadClick}
+                          >
+                            Add More
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  )
+                })}
+                <div className="row">
+                  <div className="col-sm-12">
+                    <button className="btn btn-success" type="submit">
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
-        </div>
+        )}
+        {isViewList && (
+          <div>
+            <table className="table" width="100%">
+              <thead>
+                <tr>
+                  <th scope="col">Full Name</th>
+                  <th scope="col">Date of Birth - DOB</th>
+                  <th scope="col">Age</th>
+                </tr>
+              </thead>
+              <tbody>
+                {inputList.map((x, i) => (
+                  <tr key={i}>
+                    <td>{x.fullName}</td>
+                    <td>
+                      <Moment format="MM/DD/YYYY">{x.dateOfBirth}</Moment>
+                    </td>
+                    <td>{x.age}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => {
+                setIsViewList(false)
+              }}
+            >
+              Back
+            </button>
+          </div>
+        )}
       </Container>
     </div>
   )
